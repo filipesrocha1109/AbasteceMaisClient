2b37133023b3b6331535965755e872403c41ef47
Filipe Rocha <fsrbillie@gmail.com>
2021-05-22 13:59:18 -0300

    ...

--- src/Public/Global.js
+++ src/Public/Global.js
@@ -1,6 +1,7 @@
 class Global {
     static Authorization = 'Basic QWJhc3RlY2VNYWlzOjFOJk81UWl4TiRkdw==';
     static ServerIP = 'http://servidor.construtiva.com.br/AbasteceMais/';
+    static teste = 'outro_teste'
 }
 
 export default Global
